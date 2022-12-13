39a73d2c19772ecedd7eaec32ae25377d4066545
tam <minhtriet17101@gmail.com>
2022-12-13 12:51:34 +0700

    feat(@TemplateController) : update Template Status

---
+++ DocumentTemplateAPI/DocumentTemplateModel/Entities/Templates/TemplateUpdate.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DocumentTemplateModel.Entities.Templates
+{
+    public class TemplateUpdate
+    {
+
+        public int Id { get; set; }
+        [JsonProperty(PropertyName = "status")]
+        public int Status { get; set; }
+    }
+}
